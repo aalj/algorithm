062f86a58f50634104be8d02bf84fdac715c9e9c
aalj <dqliangjun@126.com>
2017-10-10 13:39:06 +0800

    算法题  添加题目

--- algorithm/src/main/java/net/lll0/algor/AddTwoNumbers.java
+++ algorithm/src/main/java/net/lll0/algor/AddTwoNumbers.java
@@ -5,6 +5,15 @@ import net.lll0.algor.bean.ListNode;
 public class AddTwoNumbers {
 
 
+    /*
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+     */
+
     public ListNode addTwo(ListNode l1, ListNode l2) {
         //缓存当使用的数据
         ListNode be = l1;
